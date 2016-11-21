8ca4de9419e54cd93eadbf0d8925d3c2accdf3e1
robSafar <rob.safar@gmail.com>
2016-11-21 19:53:41 +0000

    Hopefully done.

---
+++ rankall.R
@@ -0,0 +1,38 @@
+rankall <- function(varOutcome, varRank = "best") {
+        ## Read outcome data
+        masterData <- read.csv("outcome-of-care-measures.csv")
+        
+        ## Check that outcome is valid
+        validOutcome <- c("heart attack","heart failure","pneumonia") == varOutcome
+        if(sum(validOutcome) < 1) {
+                stop("invalid outcome")
+        }
+        if(varOutcome == "heart attack") { masterCol <- 11 }
+        if(varOutcome == "heart failure") { masterCol <- 17 }
+        if(varOutcome == "pneumonia") { masterCol <- 23 }
+        
+        ## For each state, find the hospital of the given rank at the given outcome
+        listStates <- as.vector(unique(masterData[,7]))
+        rankings <- data.frame(hospital=character(),state=character())
+        for(i in 1:length(listStates)) {
+                
+                stateData <- subset(masterData, masterData[,7]==listStates[i])
+                x <- stateData[,masterCol] == "Not Available"
+                stateData <- stateData[!x, ]
+                stateData[,masterCol] <- as.numeric(stateData[,masterCol])
+                stateData <- stateData[ order(stateData[,masterCol], stateData[,2]), ]
+                if(varRank == "best") { 
+                        stateRank <- as.numeric(1)
+                } else {
+                        if(varRank == "worst") {
+                                stateRank <- as.numeric(nrow(stateData))
+                        } else {
+                                stateRank <- as.numeric(varRank)
+                        }
+                }
+                rankings <- rbind(rankings,stateData[stateRank,c(2,7)])
+        }
+        
+        ## Return a data frame with the hospital names and the (abbreviated) state name
+        rankings
+}
--- rankhospital.R
+++ rankhospital.R
@@ -1,8 +1,8 @@
-best <- function(varState, varOutcome) {
+rankhospital <- function(varState, varOutcome, varRank="best") {
         ## Read outcome data
         masterData <- read.csv("outcome-of-care-measures.csv")
         
-        ## Check that state and outcome are valid
+        ## Check that outcome is valid
         listStates <- unique(masterData[,7])
         validState <- listStates == varState
         if(sum(validState) < 1) {
@@ -17,12 +17,20 @@ best <- function(varState, varOutcome) {
         if(varOutcome == "heart failure") { masterCol <- 17 }
         if(varOutcome == "pneumonia") { masterCol <- 23 }
         
-        ## Return hospital name in that state with lowest 30-day death rate
+        ## Return hospital name in that state with the given rank
         stateData <- masterData[ which(masterData[,7] == varState), ]
         x <- stateData[,masterCol] == "Not Available"
         stateData <- stateData[!x, ]
         stateData[,masterCol] <- as.numeric(stateData[,masterCol])
         sortedData <- stateData[ order(stateData[,masterCol], stateData[,2]), ]
-        print(as.vector(sortedData[1,2]))
-        
+        if(varRank == "best") {
+                stateRank <- as.numeric("1")
+        } else {
+                if(varRank == "worst") {
+                        stateRank <- as.numeric(nrow(sortedData))
+                } else {
+                        stateRank <- as.numeric(varRank)
+                }
+        }
+        print(as.vector(sortedData[stateRank,2]))
 }
